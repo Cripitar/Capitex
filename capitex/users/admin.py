63e678e9392c9ea69a62c60f2f40db29cd5e07b3
Diego Diaz <104239740+Cripitar@users.noreply.github.com>
2026-05-10 01:27:45 -0500

    Delete capitex directory

--- capitex/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from django.contrib import admin
-from .models import Rol, PerfilUsuario
-
-admin.site.register(Rol)
-admin.site.register(PerfilUsuario)
--- capitex/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import RegistroView, perfil_usuario
-
-urlpatterns = [
-    path('register/', RegistroView.as_view()),
-    path('perfil/', perfil_usuario),
-]
